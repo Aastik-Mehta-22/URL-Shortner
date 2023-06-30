be0624ffb5fe2abf6f79634665ffd6e89a623353
Aastik Mehta <122990856+Aastik-Mehta-22@users.noreply.github.com>
2023-06-30 10:32:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("shorten");
+
+ btn.addEventListener('click',short);
+
+ async function short() {
+    let longURL = document.getElementById("longur1").value;
+    let shortURL = document.getElementById("shortur1");
+
+    const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longURL}`);
+    const data = await result.json();
+
+    shortURL.value = data.result.short_link2;
+    
+}
+
+
